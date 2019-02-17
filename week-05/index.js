9dcc4e380580767ceb9eab3d507c87d9acd9d65a
Aytekin Yaliz <aytekinyaliz@gmail.com>
2019-02-17 17:58:52 +0000

    Update index.js

--- week-05/index.js
+++ week-05/index.js
@@ -8,7 +8,6 @@ function getTheSecondLargest(arr) {
 
 function isLeapYearWithIfElse(year) {
    // return true/false if the given year parameter is a leap year
-   // use if/else statement
 }
 
 function isLeapYearWithSwitch(year) {
