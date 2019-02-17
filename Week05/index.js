f1fa0146c25b811ae11431dcb0b2b21946f66753
Aytekin Yaliz <aytekinyaliz@gmail.com>
2019-02-17 16:07:07 +0000

    Create index.js

---
+++ Week05/index.js
@@ -0,0 +1,17 @@
+function isPalindrome(str) {
+   // return true/false if the given string parameter is a palindrome one
+}
+
+function getTheSecondLargest(arr) {
+   // return the second largest number in the given array
+}
+
+function isLeapYearWithIfElse(year) {
+   // return true/false if the given year parameter is a leap year
+   // use if/else statement
+}
+
+function isLeapYearWithSwitch(year) {
+   // return true/false if the given year parameter is a leap year
+   // Use switch statement
+}
