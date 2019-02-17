8b99c0278d2acb9e0fbb4a67f31d19b3ec0ccbaa
Aytekin Yaliz <aytekinyaliz@gmail.com>
2019-02-17 16:02:06 +0000

    Create index.js

---
+++ Week04/index.js
@@ -0,0 +1,18 @@
+const bagetPrice = 1.05;
+const bagetQuantity = 2;
+
+const butterPrice = 2;
+const butterQuantity = 1;
+
+const milkPrice = 0.86;
+const milkQuantity = 2;
+
+let totalPriceOfTheBasket = 0;
+
+
+// Your functions will go below this line...
+
+
+console.log( formatThePrice(totalPriceOfTheBasket) );
+
+// The output should be "The total price for your basket is: £5.82"
