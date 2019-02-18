d3da59d7ac93d0ada9972c190e32eeb717ba79ef
Murphyst <murphyst@Ms-MacBook-Pro.local>
2019-02-18 16:55:47 +0000

    solutions

--- week-05/index.js
+++ week-05/index.js
@@ -1,16 +1,43 @@
 function isPalindrome(str) {
-   // return true/false if the given string parameter is a palindrome one
+   var reversedArrayOfstr = str.split("").reverse().join("");
+   if(reversedArrayOfstr === str) return true;
+     return false;
+    // return true/false if the given string parameter is a palindrome one
 }
+console.log(isPalindrome("eye"));
 
 function getTheSecondLargest(arr) {
-   // return the second largest number in the given array
+   var sortedArr = arr.sort((a, b) => b - a);
+   return sortedArr[1]
+          // return the second largest number in the given array
 }
+
+const array = [97, 90, 2, 23, 1, 10] 
+console.log(getTheSecondLargest(array));
+
 
 function isLeapYearWithIfElse(year) {
+   if(year % 4 === 0) return true;
+   else false;
    // return true/false if the given year parameter is a leap year
 }
 
+console.log(isLeapYearWithIfElse(2000));
+
 function isLeapYearWithSwitch(year) {
-   // return true/false if the given year parameter is a leap year
+   let leapYear = false;
+   switch(year % 4){
+      case 0:
+         leapYear = true;
+         break;
+      default:
+         leapYear = false;
+         break;
+   }  
+   return leapYear
+    // return true/false if the given year parameter is a leap year
    // Use switch statement
 }
+
+console.log(isLeapYearWithSwitch(1998));
+
